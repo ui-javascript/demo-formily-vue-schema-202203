32c94fba7df0b55378fae14d2fe396230c837c2f
luo0412 <1095847440@qq.com>
2022-03-22 13:20:01 +0000

    small

--- _formily-vue/demo.js
+++ _formily-vue/demo.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+
 import DemoApp from './DemoApp.vue'
 
-createApp(DemoApp).mount('#app')
+createApp(DemoApp)
+    .use(Antd)
+    .mount('#app')
