511117fa67c1f4fb7881ae4b55839b32b237c1f6
luo0412 <1095847440@qq.com>
2022-03-22 13:13:48 +0000

    adjust

--- _formily-vue/demo.js
+++ _formily-vue/demo.js
@@ -1,30 +1,4 @@
 import { createApp } from 'vue'
+import DemoApp from './DemoApp.vue'
 
-const App = {
-  template: `
-<div>
-      <span>count is {{ count }}</span>
-      <span>plusOne is {{ plusOne }}</span>
-      <button type="primary" @click="increment">count++</button>
-</div>
-  `,
-  data() {
-    return {
-      count: 0,
-    }
-  },
-  computed: {
-    plusOne: function () {
-      return this.count + 1
-    },
-  },
-  methods: {
-    increment() {
-      this.count++
-    },
-  },
-}
-
-
-
-createApp(App).mount('#app')
+createApp(DemoApp).mount('#app')
