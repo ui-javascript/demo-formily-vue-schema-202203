5ff337efc77017de26996d7e714ab27139948e2e
luo0412 <1095847440@qq.com>
2022-03-22 13:28:03 +0000

    adjust

--- _formily-vue/demo/demo.js
+++ _formily-vue/demo/demo.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 
-import DemoApp from './DemoApp.vue'
+import DemoApp from '../demo/demo/demoApp.vue'
 
 createApp(DemoApp)
     .use(Antd)
---
+++ _formily-vue/simple.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+// import Antd from 'ant-design-vue';
+// import 'ant-design-vue/dist/antd.css';
+
+import App from './simpleApp.vue'
+
+createApp(App)
+    // .use(Antd)
+    .mount('#app')
