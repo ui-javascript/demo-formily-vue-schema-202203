a58563a0e9be26a9b793e7a2a6d18e016fbeae08
luo0412 <1095847440@qq.com>
2022-03-31 11:18:07 +0000

    adjust

---
+++ _formily-vue/JSONSchema.vue
@@ -0,0 +1,29 @@
+<template>
+  <FormProvider :form="form">
+    <SchemaField :schema="schema" /> 
+  </FormProvider>
+</template>
+
+<script setup>
+import { Input } from "ant-design-vue";
+import { createForm } from "@formily/core";
+import { FormProvider, createSchemaField } from "@formily/vue";
+
+const { SchemaField } = createSchemaField({
+  components: {
+    Input,
+  },
+});
+
+const form = createForm();
+
+const schema = {
+  type: "object",
+  properties: {
+    input: {
+      type: "string",
+      "x-component": "Input",
+    },
+  },
+};
+</script>
---
+++ _formily-vue/VoidField.vue
@@ -0,0 +1,39 @@
+<template>
+  <FormProvider :form="form">
+    <Space>
+      <VoidField name="layout">
+        <Field name="input" :component="[Input]" />
+      </VoidField>
+      <FormConsumer>
+        <template #default="{ form }">
+          <Space>
+            <Button
+              @click="
+                () => {
+                  form
+                    .query('layout')
+                    .take()
+                    .setState((state) => {
+                      state.visible = !state.visible
+                    })
+                }
+              "
+            >
+              {{ form.query('layout').get('visible') ? 'Hide' : 'Show' }}
+            </Button>
+            <div>{{ JSON.stringify(form.values, null, 2) }}</div>
+          </Space>
+        </template>
+      </FormConsumer>
+    </Space>
+  </FormProvider>
+</template>
+
+<script setup>
+import { Input, Space, Button } from 'ant-design-vue'
+import { createForm } from '@formily/core'
+import { FormProvider, Field, FormConsumer, VoidField } from '@formily/vue'
+import 'ant-design-vue/dist/antd.css'
+
+const form = createForm()
+</script>
